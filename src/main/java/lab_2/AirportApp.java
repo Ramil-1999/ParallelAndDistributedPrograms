7fc2e245cd86faaf57f5837e228bd32f8dc706bf
Ramil <ramilgf16@gmail.com>
2020-11-16 15:06:11 +0300

    Scripted auto-commit on change (2020-11-16 15:06:11) by gitwatch.sh

--- src/main/java/lab_2/AirportApp.java
+++ src/main/java/lab_2/AirportApp.java
@@ -14,17 +14,17 @@ public class AirportApp {
         System.exit(-1);
     }
     Job job = Job.getInstance();
-        job.setJarByClass(WordCountApp.class);
-        job.setJobName("Word count");
+    job.setJarByClass(WordCountApp.class);
+    job.setJobName("Word count");
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        job.setNumReduceTasks(2);
+    job.setMapperClass(WordMapper.class);
+    job.setReducerClass(WordReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setNumReduceTasks(2);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
 }
