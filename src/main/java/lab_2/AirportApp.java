35f58ee39f05681c76cc50cdddef380f9c0f58a9
Ramil <ramilgf16@gmail.com>
2020-11-16 15:20:29 +0300

    Scripted auto-commit on change (2020-11-16 15:20:29) by gitwatch.sh

--- src/main/java/lab_2/AirportApp.java
+++ src/main/java/lab_2/AirportApp.java
@@ -1,7 +1,6 @@
 package lab_2;
 
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
@@ -13,19 +12,22 @@ import org.apache.hadoop.io.IntWritable;
 public class AirportApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: lab1.WordCountApp <input path> <output path>");
+            System.err.println("Usage: lab2");
             System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
         job.setJobName("AirportApp");
 
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapperAirport.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapperFlight.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setNumReduceTasks(2);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
--- src/main/java/lab_2/reducer.java
+++ src/main/java/lab_2/reducer.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.Text;
 
 import java.io.IOException;
 
-public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+public class AirportReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
