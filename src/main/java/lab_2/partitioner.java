767da8f9c1202135117dab65ea8b9885f4168a07
Ramil <ramilgf16@gmail.com>
2020-11-16 16:23:34 +0300

    Scripted auto-commit on change (2020-11-16 16:23:34) by gitwatch.sh

--- src/main/java/lab_2/partitioner.java
+++ src/main/java/lab_2/partitioner.java
@@ -2,6 +2,3 @@ package lab_2;
 
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public abstract class Partitioner<KEY, VALUE> {
-    public abstract int getPartition(KEY key, VALUE value, int numPartitions);
-}
