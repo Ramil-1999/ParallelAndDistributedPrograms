a09b108f6bc61b8792c06607e0f81fc99e05018b
Ramil <ramilgf16@gmail.com>
2020-11-16 15:11:24 +0300

    Scripted auto-commit on change (2020-11-16 15:11:24) by gitwatch.sh

--- src/main/java/lab_2/MapperAirport.java
+++ src/main/java/lab_2/MapperAirport.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
 public class MapperAirport extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
     }
