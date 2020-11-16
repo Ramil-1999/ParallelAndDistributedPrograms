3d2c808a5f0793c5c6d5b7c139d3c8aec7284c04
Ramil <ramilgf16@gmail.com>
2020-11-16 17:41:38 +0300

    Scripted auto-commit on change (2020-11-16 17:41:38) by gitwatch.sh

--- src/main/java/lab_2/AirportWritableComparable.java
+++ src/main/java/lab_2/AirportWritableComparable.java
@@ -1,5 +1,7 @@
 package lab_2;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -9,13 +11,16 @@ import java.io.IOException;
 
 public class AirportWritableComparable implements WritableComparable {
     // Some data
+    IntWritable code;
+    Text name;
 
     public void write(DataOutput out) throws IOException {
 
     }
 
     public void readFields(DataInput in) throws IOException {
-
+        code = in.readUTF();
+        name = in.readUTF();
     }
 
     public int compareTo(AirportWritableComparable o) {
