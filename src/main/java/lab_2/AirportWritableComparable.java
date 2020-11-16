7bd79f6c3100ccf66344abd8c4dd69743f9d2359
Ramil <ramilgf16@gmail.com>
2020-11-16 17:37:09 +0300

    Scripted auto-commit on change (2020-11-16 17:37:09) by gitwatch.sh

--- src/main/java/lab_2/AirportWritableComparable.java
+++ src/main/java/lab_2/AirportWritableComparable.java
@@ -1,29 +1,26 @@
 package lab_2;
 
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.IOException;
+
+
 public class AirportWritableComparable implements WritableComparable {
     // Some data
 
     public void write(DataOutput out) throws IOException {
-        out.writeInt(counter);
-        out.writeLong(timestamp);
+    
     }
 
     public void readFields(DataInput in) throws IOException {
-        counter = in.readInt();
-        timestamp = in.readLong();
+
     }
 
     public int compareTo(AirportWritableComparable o) {
-        int thisValue = this.value;
-        int thatValue = o.value;
-        return (thisValue < thatValue ? -1 : (thisValue==thatValue ? 0 : 1));
+
     }
 
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + counter;
-        result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
-        return result
+
     }
 }
