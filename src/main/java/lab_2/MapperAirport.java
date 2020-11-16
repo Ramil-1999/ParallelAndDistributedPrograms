86032c1d3b6b777ec10bbecd5ee1a83c6b9035f9
Ramil <ramilgf16@gmail.com>
2020-11-16 15:10:47 +0300

    Scripted auto-commit on change (2020-11-16 15:10:47) by gitwatch.sh

--- src/main/java/lab_2/MapperAirport.java
+++ src/main/java/lab_2/MapperAirport.java
@@ -4,10 +4,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+
 public class MapperAirport extends Mapper<LongWritable, Text, TextPair, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        ServiceCall call = new ServiceCall(value);
-        context.write(new TextPair(call.getSystemA().toString(),"1"),
-                new Text(call.toString()));
+
     }
 }
--- src/main/java/lab_2/MapperFlight.java
+++ src/main/java/lab_2/MapperFlight.java
@@ -4,8 +4,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+
 public class MapperAirport extends Mapper<LongWritable, Text, TextPair, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-       
+
     }
 }
