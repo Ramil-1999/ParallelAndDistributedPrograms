c0e4ee539eeb507777a2133e742c80ec8b7321f5
Ramil <ramilgf16@gmail.com>
2020-11-16 15:12:59 +0300

    Scripted auto-commit on change (2020-11-16 15:12:59) by gitwatch.sh

--- src/main/java/lab_2/reducer.java
+++ src/main/java/lab_2/reducer.java
@@ -1,4 +1,14 @@
 package lab_2;
 
-public class reducer {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        }
+    }
 }
