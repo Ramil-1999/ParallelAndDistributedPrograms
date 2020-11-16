147a21a0ca1193f84fb75e50918d00d3f59bc46e
Ramil <ramilgf16@gmail.com>
2020-11-16 17:57:17 +0300

    Scripted auto-commit on change (2020-11-16 17:57:17) by gitwatch.sh

--- src/main/java/lab_2/AirportReducer.java
+++ src/main/java/lab_2/AirportReducer.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        
+
     }
 }
