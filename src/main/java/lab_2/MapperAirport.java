54176313d3193aaa9916b0280a9a9064dacafc1f
Ramil <ramilgf16@gmail.com>
2020-11-16 15:08:56 +0300

    Scripted auto-commit on change (2020-11-16 15:08:56) by gitwatch.sh

--- src/main/java/lab_2/MapperAirport.java
+++ src/main/java/lab_2/MapperAirport.java
@@ -2,6 +2,7 @@ package lab_2;
 
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
 public class MapperAirport extends Mapper<LongWritable, Text, TextPair, Text> {
 
