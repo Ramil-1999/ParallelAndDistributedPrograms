983dc8481353a15edd1fa255f0ddb11ba6005a66
Ramil <ramilgf16@gmail.com>
2020-11-16 15:07:30 +0300

    Scripted auto-commit on change (2020-11-16 15:07:30) by gitwatch.sh

--- src/main/java/lab_2/AirportApp.java
+++ src/main/java/lab_2/AirportApp.java
@@ -9,22 +9,19 @@ import org.apache.hadoop.io.IntWritable;
 
 
 public class AirportApp {
-    if (args.length != 2) {
-        System.err.println("Usage: lab1.WordCountApp <input path> <output path>");
-        System.exit(-1);
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: lab1.WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(AirportApp.class);
+        job.setJobName("AirportApp");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-    Job job = Job.getInstance();
-    job.setJarByClass(WordCountApp.class);
-    job.setJobName("Word count");
-
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-    job.setMapperClass(WordMapper.class);
-    job.setReducerClass(WordReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setNumReduceTasks(2);
-
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
 }
