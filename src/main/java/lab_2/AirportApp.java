4b4ea1fba6ede7616fea43200a537265a140676c
Ramil <ramilgf16@gmail.com>
2020-11-17 11:57:15 +0300

    Scripted auto-commit on change (2020-11-17 11:57:15) by gitwatch.sh

--- src/main/java/lab_2/AirportApp.java
+++ src/main/java/lab_2/AirportApp.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.IntWritable;
 
 
 public class AirportApp {
