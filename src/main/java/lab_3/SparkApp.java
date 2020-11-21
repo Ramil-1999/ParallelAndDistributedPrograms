c175c21860c6e2ff78708d47311b66c114f35853
Ramil <ramilgf16@gmail.com>
2020-11-21 03:39:31 +0300

    Scripted auto-commit on change (2020-11-21 03:39:31) by gitwatch.sh

--- src/main/java/lab_3/SparkApp.java
+++ src/main/java/lab_3/SparkApp.java
@@ -1,8 +1,16 @@
 package lab_3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkApp {
     public static void main(String[] args) throws Exception {
+        //Spark init
+        SparkConf conf = new SparkConf().setAppName("SparkApp");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        //загрузка данных
+
 
     }
 }
