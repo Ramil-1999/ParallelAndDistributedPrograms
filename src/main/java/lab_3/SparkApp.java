7867b17e9d4919fec48ff725ed1d4ba8ebc127cd
Ramil <ramilgf16@gmail.com>
2020-11-22 16:59:19 +0300

    Scripted auto-commit on change (2020-11-22 16:59:19) by gitwatch.sh

--- src/main/java/lab_3/SparkApp.java
+++ src/main/java/lab_3/SparkApp.java
@@ -1,19 +1,21 @@
 package lab_3;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+
 
 public class SparkApp {
     public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: lab3 <table file> <dictionary file> <output file>");
+            System.exit(-1);
+        }
+
         //Spark init
-        SparkConf conf = new SparkConf().setAppName("SparkApp");
+        SparkConf conf = new SparkConf().setAppName("SparkApp").set("spark.driver.host","127.0.0.1").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        //загрузка данных из HDFS
-        JavaRDD<String> airports = sc.textFile("hadoop/2.csv");
-        JavaRDD<String> flights = sc.textFile("hadoop/1.csv");
-
-        System.out.println(airports);
+        JavaR
     }
 }
