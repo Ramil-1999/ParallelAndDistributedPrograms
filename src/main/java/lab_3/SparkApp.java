83adc6757591b26ea4a60c534e5921313b658611
Ramil <ramilgf16@gmail.com>
2020-11-24 01:32:39 +0300

    Scripted auto-commit on change (2020-11-24 01:32:39) by gitwatch.sh

--- src/main/java/lab_3/SparkApp.java
+++ src/main/java/lab_3/SparkApp.java
@@ -52,8 +52,8 @@ public class SparkApp {
 
         Map<String, String> airportMap = airports.map(row -> {
             String[] values = row.split(REGEX);
-            
-        });
+            return values[1];
+        }).collectAsMap();
 
 
         final Broadcast<Map<String, String>> airportBroadcasted = sc.broadcast();
