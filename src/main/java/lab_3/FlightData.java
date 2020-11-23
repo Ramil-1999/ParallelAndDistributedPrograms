de072e4b7a9b5643bbdd64c115ab7b27cfca435f
Ramil <ramilgf16@gmail.com>
2020-11-24 01:52:56 +0300

    Scripted auto-commit on change (2020-11-24 01:52:56) by gitwatch.sh

--- src/main/java/lab_3/FlightData.java
+++ src/main/java/lab_3/FlightData.java
@@ -32,10 +32,15 @@ public class FlightData implements Serializable {
         int counterSize = 0;
         for (FlightData flight: flights){
             counterSize++;
-            Double cDelay = Double.parseDouble(flight.getDelay());
             Double cIsCancelled = Double.parseDouble(flight.getIsCancelled());
+            if (cIsCancelled == 1){
+                counterCanceled++;
+                continue;
+            }
+            Double cDelay = Double.parseDouble(flight.getDelay());
             if (cDelay > maxDelay) maxDelay = cDelay;
-            if (cIsCancelled == 1 || cDelay > 0) counterCanceled++;
+            if (cDelay > 0) counterCanceled++;
+            
         }
         FlightData tmp = new FlightData(String.valueOf(maxDelay), String.valueOf(100 * (double)counterCanceled/(double)counterSize));
         return tmp;
